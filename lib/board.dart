6c33186e0450059248dd4a351b19e4dc33b43880
darlingson <dmakuwila@gmail.com>
2024-01-30 11:21:24 +0200

    added player indicator

--- lib/board.dart
+++ lib/board.dart
@@ -15,16 +15,19 @@ class _GameBoardState extends State<GameBoard> {
       List.generate(32, (index) => Stone(imagePath: 'lib/assets/bawo.jpg'));
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-            child: GridView.builder(
-      itemCount: 32,
-      physics: const NeverScrollableScrollPhysics(),
-      gridDelegate:
-          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8),
-      itemBuilder: (context, index) => BawoPit(
-        stone: stones[index],
-      ),
-    )));
+    return Container(
+      height: 200,
+      width: 400,
+      child: Center(
+          child: GridView.builder(
+        itemCount: 32,
+        physics: const NeverScrollableScrollPhysics(),
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8),
+        itemBuilder: (context, index) => BawoPit(
+          stone: stones[index],
+        ),
+      )),
+    );
   }
 }
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -2,20 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:bawo_game/board.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Bawo'),
-        ),
-        body: const Board());
+      appBar: AppBar(
+        title: const Text('Bawo'),
+      ),
+      body: const Board(),
+    );
   }
 }
 
 class Board extends StatefulWidget {
-  const Board({super.key});
+  const Board({Key? key}) : super(key: key);
 
   @override
   State<Board> createState() => _BoardState();
@@ -24,9 +25,40 @@ class Board extends StatefulWidget {
 class _BoardState extends State<Board> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: GameBoard(),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment:
+            CrossAxisAlignment.stretch, // Make children stretch horizontally
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(),
+            ),
+            height: 50,
+            child: const Center(
+              child: Text(
+                "Computer",
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+          const Center(
+            child: GameBoard(),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(),
+            ),
+            height: 50,
+            child: const Center(
+              child: Text(
+                "You",
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }

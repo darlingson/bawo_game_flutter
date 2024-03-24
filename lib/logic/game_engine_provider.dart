db4c44caafcc8a3ce4ffb55ec8086f6e3a2c4800
darlingson <dmakuwila@gmail.com>
2024-03-24 15:59:24 +0200

    removed duplicate list to hold number of stones in pits

--- lib/logic/game_engine_provider.dart
+++ lib/logic/game_engine_provider.dart
@@ -6,40 +6,6 @@ class GameEngineProvider with ChangeNotifier {
   int currentPlayer = 1;
   late GameEngine gameEngine;
   final List<int> moveIndex = [];
-  final List<int> pitIndexes = [
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2
-  ];
   GameEngineProvider() {
     gameEngine = GameEngine(this);
   }

db487dd09138115ec0c63dd8d2f70ee7196b8b38
darlingson <dmakuwila@gmail.com>
2024-03-24 16:16:19 +0200

    created a comp player class

---
+++ lib/logic/comp_player.dart
@@ -0,0 +1,7 @@
+import 'package:bawo_game/logic/game_engine_provider.dart';
+
+class CompPlayer {
+  GameEngineProvider gameEngineProvider = GameEngineProvider();
+  void calculateMove() {}
+  void makeMove() {}
+}
